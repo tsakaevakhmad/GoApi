249a4aae584875ac0d1d37179f117f7a6814d320
ZLUKADARK <ahmadcakaev@mail.ru>
2024-07-26 21:53:23 +0600

    Added db queries

--- internal/datalayer/repositories/AlbumRepository.go
+++ internal/datalayer/repositories/AlbumRepository.go
@@ -17,17 +17,11 @@ func NewAlbumRepository(driver string, connection string) *AlbumRepository {
 	}
 }
 
-var albums = []domain.Album{
-	{Id: 1, Price: 13.99, Artist: "Oleg F. A.", Name: "Her'"},
-	{Id: 2, Price: 10.99, Artist: "Manis K. K.", Name: "YouNga"},
-	{Id: 3, Price: 9.99, Artist: "Alister K. E.", Name: "Shorts"},
-	{Id: 4, Price: 2.99, Artist: "Capitan K. E.", Name: "Manual"},
-}
-
 func (ar *AlbumRepository) Get(id int64) *domain.Album {
 	var album domain.Album
 	query := `SELECT * FROM albums WHERE id = $1`
 	ar.context.QueryRow(query, id).Scan(&album.Id, &album.Price, &album.Artist, &album.Name)
+	ar.context.Close()
 	return &album
 }
 
@@ -51,6 +45,7 @@ func (ar *AlbumRepository) GetAll() []domain.Album {
 
 		albums = append(albums, album)
 	}
+	ar.context.Close()
 	return albums
 }
 
@@ -60,6 +55,7 @@ func (ar *AlbumRepository) Create(data domain.Album) *domain.Album {
 	if err != nil {
 		log.Fatal(err)
 	}
+	ar.context.Close()
 	return &data
 }
 
@@ -67,11 +63,13 @@ func (ar *AlbumRepository) Delete(id int64) *domain.Album {
 	var album domain.Album
 	query := `DELETE FROM albums WHERE id = $1 RETURNING *`
 	ar.context.QueryRow(query, id).Scan(&album.Id, &album.Price, &album.Artist, &album.Name)
+	ar.context.Close()
 	return &album
 }
 
 func (ar *AlbumRepository) Put(data domain.Album) *domain.Album {
 	query := `UPDATE albums SET price=$1, artist=$2, name=$3 WHERE id=$4 RETURNING *`
 	ar.context.QueryRow(query, data.Price, data.Artist, data.Name, data.Id).Scan(&data.Id, &data.Price, &data.Artist, &data.Name)
+	ar.context.Close()
 	return &data
 }
